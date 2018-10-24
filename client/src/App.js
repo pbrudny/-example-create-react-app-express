419b09fd93c8048ac9d3991812408a5abbc35269
pbrudny <pbrudny@gmail.com>
2018-10-24 16:59:59 +0100

    Add basic fullstack app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
@@ -20,6 +38,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <p className="App-intro">{this.state.response}</p>
       </div>
     );
   }

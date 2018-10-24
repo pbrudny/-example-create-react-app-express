b5d05dfd4362d04f74c526edce808f62524ef1a1
pbrudny <pbrudny@gmail.com>
2018-10-24 18:23:02 +0100

    Add db

---
+++ README.md
@@ -0,0 +1,9 @@
+# Example Create React App with Express and MongoDB
+Test using Postman
+
+    localhost:5000/products/create
+
+    Example:
+    Body
+    name apple
+    price 10
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+// https://codeburst.io/writing-a-crud-app-with-node-js-and-mongodb-e0827cbbdafb
+
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://someuser:abcd1234@ds123619.mlab.com:23619/productstutorial';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 const app = express();
 const port = process.env.PORT || 5000;
 // API calls
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const product = require('./routes/product.route');
+
+app.use('/products', product);
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
